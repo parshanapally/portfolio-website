e6f1623968c7cef086342efe581d95c7f94f6003
Pooja Arshanapally <parshanapally@qca-sdc.com>
2019-01-26 11:20:59 -0600

    test: fix test of portfolio

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -1,11 +1,16 @@
 import React from "react";
 import "./index.css";
+import mainBuzz from "../img/main-buzz.png";
 
 const Portfolio = () => {
   return (
     <div className="container-portfolio">
       <div className="container-fluid">
-        <div className="container-box">Project 1</div>
+        <div className="container-box">
+          <a href="#">
+            <img src={mainBuzz} />
+          </a>
+        </div>
         <div className="container-box">Project 2</div>
         <div className="container-box">Project 3</div>
       </div>
--- src/components/Portfolio/index.test.js
+++ src/components/Portfolio/index.test.js
@@ -6,7 +6,22 @@ import Portfolio from "../Portfolio";
 describe("Given Contact ", () => {
   const component = shallow(<Portfolio />);
 
-  it("should exist", () => {
-    expect(component.find("div")).to.have.length(1);
+  it("should contain div with classname of container-portfolio ", () => {
+    expect(component.find("div.container-portfolio")).to.have.length(1);
+  });
+
+  it("should contain div with classname of container-fluid ", () => {
+    expect(component.find("div.container-fluid")).to.have.length(1);
+  });
+  it("should contain div with classname of container-box ", () => {
+    expect(component.find("div.container-box")).to.have.length(3);
+  });
+
+  it("should contain a", () => {
+    expect(component.find("a")).to.have.length(1);
+  });
+
+  it("should contain img", () => {
+    expect(component.find("img")).to.have.length(1);
   });
 });
