617dcccb0ae171cede4f82b139faca497aeeca7e
Pooja Arshanapally <parshanapally@qca-sdc.com>
2019-01-26 11:29:20 -0600

    test: fix test of home

--- src/components/Home/index.test.js
+++ src/components/Home/index.test.js
@@ -6,10 +6,22 @@ import Home from "../Home";
 describe("Given Contact ", () => {
   const component = shallow(<Home />);
 
-  it("should exist", () => {
-    expect(component.find("div")).to.have.length(1);
+  it("should contain div with classname container", () => {
+    expect(component.find("div.container")).to.have.length(2);
   });
 
+  it("should contain div with id header", () => {
+    expect(component.find("div#header")).to.have.length(1);
+  });
+  it("should contain div with id description", () => {
+    expect(component.find("div#description")).to.have.length(1);
+  });
+  it("should contain h1", () => {
+    expect(component.find("h1")).to.have.length(1);
+  });
+  it("should contain h2", () => {
+    expect(component.find("h2")).to.have.length(1);
+  });
   it("should exist", () => {
     expect(component.find("p")).to.have.length(1);
   });
