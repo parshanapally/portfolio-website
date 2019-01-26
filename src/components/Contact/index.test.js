868fb243cb3af8c5c6f7fd75a1e1d7c50388aaf4
Pooja Arshanapally <parshanapally@qca-sdc.com>
2019-01-26 11:24:47 -0600

    test: fix test of contact

--- src/components/Contact/index.test.js
+++ src/components/Contact/index.test.js
@@ -6,17 +6,22 @@ import Contact from "../Contact";
 describe("Given Contact ", () => {
   const component = shallow(<Contact />);
 
-  it("should exist", () => {
+  it("should contain div with classname of container-fluid ", () => {
     expect(component.find("div.container-fluid")).to.have.length(1);
   });
-  it("should exist", () => {
+  it("should contain div with classname of row", () => {
     expect(component.find("div.row")).to.have.length(1);
   });
-  it("should exist", () => {
+  it("should contain div with classname of personal-info", () => {
     expect(component.find("div.personal-info")).to.have.length(1);
   });
-  it("should exist", () => {
+  it("should contain a", () => {
     const paragraph = component.find("p.social-media");
     expect(paragraph.find("a")).to.have.length(3);
+  });
+
+  it("should contain img", () => {
+    const paragraph = component.find("p.social-media");
+    expect(paragraph.find("img")).to.have.length(3);
   });
 });
