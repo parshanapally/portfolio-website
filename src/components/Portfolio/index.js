9fd8091d9ad5365c8b747322697961246f3b471b
Pooja Arshanapally <parshanapally@qca-sdc.com>
2019-01-26 13:00:49 -0600

    style: add two pictures in portfolio

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -1,17 +1,24 @@
 import React from "react";
 import "./index.css";
 import mainBuzz from "../img/main-buzz.png";
+import pdfDoc from "../img/IM261pdf.pdf";
+import techAppDoc from "../img/tech-app.pdf";
+import techApp from "../img/tech-app.png";
 
 const Portfolio = () => {
   return (
     <div className="container-portfolio">
       <div className="container-fluid">
         <div className="container-box">
-          <a href="#">
+          <a href={pdfDoc} download>
             <img src={mainBuzz} />
           </a>
         </div>
-        <div className="container-box">Project 2</div>
+        <div className="container-box">
+          <a href={techAppDoc} download>
+            <img src={techApp} />
+          </a>
+        </div>
         <div className="container-box">Project 3</div>
       </div>
     </div>
