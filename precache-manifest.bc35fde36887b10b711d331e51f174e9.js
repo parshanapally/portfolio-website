d873781a22163ce1e78b539914ee27282409828a
Pooja Arshanapally <parshanapally@qca-sdc.com>
2019-01-26 13:27:05 -0600

    Updates

---
+++ precache-manifest.bc35fde36887b10b711d331e51f174e9.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "c3bfd99f2252c676de6fc410bc2e44bb",
+    "url": "/portfolio-website/static/media/IM261pdf.c3bfd99f.pdf"
+  },
+  {
+    "revision": "d9f19f470f7c6146b4e6",
+    "url": "/portfolio-website/static/css/main.a35d697e.chunk.css"
+  },
+  {
+    "revision": "b88e383f2a43bdf2ff13",
+    "url": "/portfolio-website/static/js/1.b88e383f.chunk.js"
+  },
+  {
+    "revision": "2b256896c0c48cf1d86c",
+    "url": "/portfolio-website/static/js/runtime~main.2b256896.js"
+  },
+  {
+    "revision": "f6acd30409a75b40bd7f5cd30c7f18e9",
+    "url": "/portfolio-website/static/media/main-buzz.f6acd304.png"
+  },
+  {
+    "revision": "d9f19f470f7c6146b4e6",
+    "url": "/portfolio-website/static/js/main.d9f19f47.chunk.js"
+  },
+  {
+    "revision": "32f024d3389f28d2531d8834fac14ba9",
+    "url": "/portfolio-website/static/media/tech-app.32f024d3.pdf"
+  },
+  {
+    "revision": "53ed5629d00d4904b39eae35de44ab45",
+    "url": "/portfolio-website/static/media/tech-app.53ed5629.png"
+  },
+  {
+    "revision": "33b95d0613636bd7af9ef792757cf70e",
+    "url": "/portfolio-website/static/media/linkedin.33b95d06.png"
+  },
+  {
+    "revision": "9344b4f0a2be09638768197a3cdd5918",
+    "url": "/portfolio-website/static/media/instagram.9344b4f0.png"
+  },
+  {
+    "revision": "b75fa0151b9837dfd28620f683f99fa7",
+    "url": "/portfolio-website/index.html"
+  }
+];
