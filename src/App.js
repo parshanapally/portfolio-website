303885c07e92d6bdf7325636cc70223a3e7d701e
Pooja Arshanapally <parshanapally@qca-sdc.com>
2019-04-30 10:08:38 -0500

    chore: change router

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import React, { Component } from "react";
 import "./App.css";
 import Portfolio from "./components/Portfolio";
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+  history
+} from "react-router-dom";
 import FirstItem from "./components/FirstItem/index";
 import SecondItem from "./components/SecondItem/index";
 import ThirdItem from "./components/ThirdItem/index";
@@ -12,12 +17,12 @@ class App extends Component {
     return (
       <Router>
         <main>
-          <Route exact path="/portfolio-website/" component={Portfolio} />
           <Route
             exact
-            path="/portfolio-website/bridging-digital-divide"
-            component={FirstItem}
+            path={process.env.PUBLIC_URL + "/portfolio-website"}
+            component={Portfolio}
           />
+          <Route exact path="/bridging-digital-divide" component={FirstItem} />
           <Route exact path="/rewards" component={SecondItem} />
           <Route exact path="/to-do-app" component={ThirdItem} />
           <Route exact path="/tech-app" component={FourthItem} />
