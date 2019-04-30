1a25f44f312df11cb9fdd456c431a86209eb39c2
Pooja Arshanapally <parshanapally@qca-sdc.com>
2019-04-30 10:28:48 -0500

    feat: change to Hashrouter

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import "./App.css";
 import Portfolio from "./components/Portfolio";
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { HashRouter as Router, Route, Link } from "react-router-dom";
 import FirstItem from "./components/FirstItem/index";
 import SecondItem from "./components/SecondItem/index";
 import ThirdItem from "./components/ThirdItem/index";
