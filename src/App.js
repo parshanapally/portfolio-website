d51c6bb050e4bfaaac895bc33f9aa0b2d286dbce
Pooja Arshanapally <parshanapally@qca-sdc.com>
2019-05-01 13:23:35 -0500

    chore: change back to browser router

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import "./App.css";
 import Portfolio from "./components/Portfolio";
-import { HashRouter as Router, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import FirstItem from "./components/FirstItem/index";
 import SecondItem from "./components/SecondItem/index";
 import ThirdItem from "./components/ThirdItem/index";
@@ -13,10 +13,10 @@ class App extends Component {
       <Router>
         <main>
           <Route exact path="/" component={Portfolio} />
-          <Route exact path="/bridging-digital-divide" component={FirstItem} />
-          <Route exact path="/rewards" component={SecondItem} />
-          <Route exact path="/to-do-app" component={ThirdItem} />
-          <Route exact path="/tech-app" component={FourthItem} />
+          <Route exact path="/bridging-digital-divide/" component={FirstItem} />
+          <Route exact path="/rewards/" component={SecondItem} />
+          <Route exact path="/to-do-app/" component={ThirdItem} />
+          <Route exact path="/tech-app/" component={FourthItem} />
         </main>
       </Router>
     );
